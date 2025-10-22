2ad43b96d34d5ab006f1a8b81a6f668dc3f944f9
iqbalgany <rismagany1203@gmail.com>
2025-10-22 17:56:42 +0700

    feat: update detail photo page

--- lib/presentation/pages/detail_photo_page.dart
+++ lib/presentation/pages/detail_photo_page.dart
@@ -1,5 +1,7 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:photo_idea_app/common/enums.dart';
 import 'package:photo_idea_app/presentation/controllers/detail_photo_controller.dart';
 
 class DetailPhotoPage extends StatefulWidget {
@@ -33,6 +35,78 @@ class _DetailPhotoPageState extends State<DetailPhotoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Obx(
+        () {
+          final state = detailPhotoController.state;
+          if (state.fetchStatus == FetchStatus.init) {
+            return const SizedBox();
+          }
+          if (state.fetchStatus == FetchStatus.loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state.fetchStatus == FetchStatus.failed) {
+            return Center(child: Text(state.message));
+          }
+          final photo = state.data!;
+          return ListView(
+            padding: EdgeInsets.all(0),
+            children: [
+              AspectRatio(
+                aspectRatio: 0.8,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    buildImage(photo.source?.large2X ?? ''),
+                    Positioned(
+                      top: 60,
+                      left: 16,
+                      right: 16,
+                      child: Row(
+                        children: [
+                          buildBackButton(),
+                          Spacer(),
+                          buildPreview(),
+                          buildSaveButton(),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildImage(String imageUrl) {
+    return ExtendedImage.network(
+      imageUrl,
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget buildBackButton() {
+    return BackButton(
+      color: Colors.white,
+      style: ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll(Colors.black38),
+      ),
+    );
+  }
+
+  Widget buildPreview() {
+    return IconButton(onPressed: () {}, icon: Icon(Icons.visibility));
+  }
+
+  Widget buildSaveButton() {
+    return BackButton(
+      color: Colors.white,
+      style: ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll(Colors.black38),
+      ),
+    );
   }
 }
