634856a0905344f0b1f08e81be19156e016c6ef8
iqbalgany <rismagany1203@gmail.com>
2025-10-28 12:12:46 +0700

    feat: add local data source implementation

--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,5 +1,6 @@
 import 'package:fd_log/fd_log.dart';
 import 'package:get_it/get_it.dart';
+import 'package:photo_idea_app/data/db/database_helper.dart';
 
 GetIt sl = GetIt.instance;
 
@@ -11,5 +12,7 @@ initInjection() {
     maxRow: 5,
     prefix: '>',
   );
+
   sl.registerLazySingleton(() => fdLog);
+  sl.registerLazySingleton(() => DatabaseHelper());
 }
---
+++ lib/data/datasources/local_photo_data_source.dart
@@ -0,0 +1,63 @@
+import 'package:photo_idea_app/core/di.dart';
+import 'package:photo_idea_app/data/db/database_helper.dart';
+import 'package:photo_idea_app/data/models/photo_model.dart';
+
+class LocalPhotoDataSource {
+  static Future<(bool, String, List<PhotoModel>?)> fetchAll() async {
+    try {
+      final database = await sl<DatabaseHelper>().database;
+      final list = await database.query('saved');
+      final photos = list.map((e) => PhotoModel.fromJsonSaved(e)).toList();
+      return (true, "Fetch Success", photos);
+    } catch (e) {
+      return (false, "Something went wrong", null);
+    }
+  }
+
+  static Future<(bool, String, bool?)> checkIsSave(int id) async {
+    try {
+      final database = await sl<DatabaseHelper>().database;
+      final list = await database.query(
+        'saved',
+        where: 'id=?',
+        whereArgs: [id],
+      );
+      return (true, 'Success Check', list.isNotEmpty);
+    } catch (e) {
+      return (false, 'Something went wrong', null);
+    }
+  }
+
+  static Future<(bool, String)> savePhoto(PhotoModel photo) async {
+    try {
+      final database = await sl<DatabaseHelper>().database;
+      final rowAffected = await database.insert(
+        'saved',
+        photo.toJsonSaved(),
+      );
+      if (rowAffected != 0) {
+        return (true, 'Photo has saved');
+      }
+      return (false, 'Photo has not saved');
+    } catch (e) {
+      return (false, 'Something went wrong');
+    }
+  }
+
+  static Future<(bool, String)> unsavePhoto(int id) async {
+    try {
+      final database = await sl<DatabaseHelper>().database;
+      final rowAffected = await database.delete(
+        'saved',
+        where: 'id=?',
+        whereArgs: [id],
+      );
+      if (rowAffected != 0) {
+        return (true, 'Remove from saved');
+      }
+      return (false, 'Failed to unsaved');
+    } catch (e) {
+      return (false, 'Something went wrong');
+    }
+  }
+}
